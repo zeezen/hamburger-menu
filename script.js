383d1302516aff1bd86a56d1f9b98a55ee8a1750
zeezen <163298731+zeezen@users.noreply.github.com>
2024-04-04 11:00:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function tampilsidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function hilangsidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
